2144ecebf8f657321468c71f21cd3c7ae1f8418f
Harsha7899 <43595573+Harsha7899@users.noreply.github.com>
2025-01-20 22:58:18 -0500

    Time: 127 ms (8.37%) | Memory: 46.3 MB (7.27%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+            String fixed_String = "";
+            
+        for (char c : s.toCharArray())
+        {
+            if(Character.isDigit(c)|| Character.isLetter(c))
+            {
+                fixed_String += c;
+            }
+        }
+        
+        fixed_String = fixed_String.toLowerCase();
+
+        int left =0;
+        int right =  fixed_String.length()-1;
+        
+        while( left <= right)
+        {
+            if( fixed_String.charAt(left) != fixed_String.charAt(right))
+              {
+                     return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
