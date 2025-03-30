f18d315f171b952e3423fec5705631b4adf3891d
Sri Harsha Mattaparthy <43595573+Harsha7899@users.noreply.github.com>
2025-03-30 10:08:02 -0400

    Time: 0 ms (100.00%) | Memory: 45.1 MB (7.10%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,16 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode single_pointer =head;
-        ListNode double_pointer =head;
+        ListNode single_pointer = head;
+        ListNode double_pointer = head;
 
         if (head == null)
         return false;
 
         while(double_pointer != null && double_pointer.next != null){
-            double_pointer= double_pointer.next.next;
-            single_pointer= single_pointer.next;
-            if(double_pointer== single_pointer)
+            double_pointer = double_pointer.next.next;
+            single_pointer = single_pointer.next;
+            if(double_pointer == single_pointer)
             return true;
         }
         return false;
