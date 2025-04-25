56c4bfacd8adaaef627f693cdfe6258a23eea005
Sri Harsha Mattaparthy <43595573+Harsha7899@users.noreply.github.com>
2025-04-25 16:00:29 -0400

    Time: 4 ms (100.00%) | Memory: 44.4 MB (26.32%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+     
+        if(x < 0)  return false;
+
+        int original_number = x, sum =0,rev;
+        while(x != 0)
+        {
+           sum = (sum * 10) + x % 10;
+           x/=10;
+        }
+        return original_number == sum;
+    }
+}
