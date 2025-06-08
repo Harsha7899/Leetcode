ff5f9c83383afbec1710147bd63cf9cfae29b213
Sri Harsha Mattaparthy <43595573+Harsha7899@users.noreply.github.com>
2025-06-08 15:32:45 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (56.80%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -13,18 +13,15 @@ class Solution:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
 
-            #adding sum
             val = v1 + v2 + carry
-            carry = val // 10
+            carry = val //10
             val = val % 10
             cur.next = ListNode(val)
 
-            #update pointers
+            #update ptrs
             cur = cur.next
             l1 = l1.next if l1 else 0
             l2 = l2.next if l2 else 0
+
         return dummy.next
-
-
-
 
